1e3c40eee81204fbf9d7adb45e50b7d634d4e944
joshclement0 <joshclement0@gmail.com>
2018-10-04 00:38:32 +0000

    game working

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 /* global $ */
 $(document).ready(function() {
+    var dateArray=[];
+    var months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+    var url = "https://holidayapi.com/v1/holidays?&key=7f1ed9ac-b8bc-4380-adb9-bdf78e5ace25&year=2017&country=US";
     var HolidayName;
+    var HolidayMonth;
+    $.getJSON(url,function(data) {
+         for(x in data["holidays"]) {
+             dateArray.push(x);
+         }
+         });
     function getRandomInt(max){
         return Math.floor(Math.random()*Math.floor(max));
     }
     $("#newGameButton").click(function(e){
-        var value =$("#monthField").val();
         e.preventDefault();
         console.log("click recieved")
         $.getJSON(url,function(data){
@@ -15,8 +23,22 @@ $(document).ready(function() {
             console.log(dateArray[randomNum])
             console.log(data["holidays"][dateArray[randomNum]][0]["name"])
             HolidayName=data["holidays"][dateArray[randomNum]][0]["name"];
-            console.log(HolidayName)
+            HolidayMonth=months[dateArray[randomNum].substr(5,2)-1];
+            console.log(HolidayMonth)
             $("#Holiday").html(HolidayName);
         })
     });
+    $("#submitButton").click(function(e){
+        e.preventDefault();
+        var value =$("#monthField").val();
+        console.log(value+" "+HolidayMonth)
+        if (value==HolidayMonth){
+            console.log("same")
+            $("#responseToGame").html("Congratulations")
+        }
+        else{
+            $("#responseToGame").html("Incorrect")
+            console.log(" -"+value+"- -"+HolidayMonth+"- ")
+        }
+    })
 });
