bd3a4994af5724bfd2ee8db21e86fa34a3991067
joshclement0 <joshclement0@gmail.com>
2018-10-05 00:01:44 +0000

    added games.css file

--- game.js
+++ game.js
@@ -1,44 +1,56 @@
 /* global $ */
 $(document).ready(function() {
-    var dateArray=[];
-    var months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+    var dateArray = [];
+    var monthGuess="";
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     var url = "https://holidayapi.com/v1/holidays?&key=7f1ed9ac-b8bc-4380-adb9-bdf78e5ace25&year=2017&country=US";
     var HolidayName;
     var HolidayMonth;
-    $.getJSON(url,function(data) {
-         for(x in data["holidays"]) {
-             dateArray.push(x);
-         }
-         });
-    function getRandomInt(max){
-        return Math.floor(Math.random()*Math.floor(max));
+    $.getJSON(url, function(data) {
+        for (x in data["holidays"]) {
+            dateArray.push(x);
+        }
+    });
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
-    $("#newGameButton").click(function(e){
+    $("#newGameButton").click(function(e) {
+        $("#congrats").style.width="0"
         e.preventDefault();
         console.log("click recieved")
-        $.getJSON(url,function(data){
-            var randomNum=getRandomInt(dateArray.length);
+        $.getJSON(url, function(data) {
+            var randomNum = getRandomInt(dateArray.length);
             console.log(randomNum)
             console.log(dateArray.length)
             console.log(dateArray[randomNum])
             console.log(data["holidays"][dateArray[randomNum]][0]["name"])
-            HolidayName=data["holidays"][dateArray[randomNum]][0]["name"];
-            HolidayMonth=months[dateArray[randomNum].substr(5,2)-1];
+            HolidayName = data["holidays"][dateArray[randomNum]][0]["name"];
+            HolidayMonth = months[dateArray[randomNum].substr(5, 2) - 1];
             console.log(HolidayMonth)
             $("#Holiday").html(HolidayName);
         })
     });
-    $("#submitButton").click(function(e){
+    $("#submitButton").click(function(e) {
         e.preventDefault();
-        var value =$("#monthField").val();
-        console.log(value+" "+HolidayMonth)
-        if (value==HolidayMonth){
+        var value = $("#monthField").val();
+        console.log(value + " " + HolidayMonth)
+        if (value == HolidayMonth) {
             console.log("same")
             $("#responseToGame").html("Congratulations")
+            $("#congrats").style.width="100%"
         }
-        else{
+        else {
             $("#responseToGame").html("Incorrect")
-            console.log(" -"+value+"- -"+HolidayMonth+"- ")
+            console.log(" -" + value + "- -" + HolidayMonth + "- ")
         }
     })
+    function changeMonthGuess(x){
+        monthGuess=months[x];
+        console.log(monthGuess)
+    }
+    function setJanuary(){
+        monthGuess=months[0];
+    }
 });
+
